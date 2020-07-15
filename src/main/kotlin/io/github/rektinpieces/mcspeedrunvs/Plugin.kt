3b17283139f99922ade29ac11517beec3f1e7112
Kevin Liu <kevin@kliu.io>
2020-07-15 13:05:13 -0400

    Add build compilation and test

---
+++ src/main/kotlin/io/github/rektinpieces/mcspeedrunvs/Plugin.kt
@@ -0,0 +1,9 @@
+package io.github.rektinpieces.mcspeedrunvs
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Plugin : JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Enabled MCSpeedrunVS")
+    }
+}

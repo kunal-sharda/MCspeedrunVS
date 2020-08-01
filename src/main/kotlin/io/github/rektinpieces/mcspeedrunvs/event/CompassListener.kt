026588f441ea192038ed695ac5c8cacf0ede4de2
RektInPieces <kunalsharda@live.com>
2020-08-01 17:34:58 -0400

    Compass Listner WIP

---
+++ src/main/kotlin/io/github/rektinpieces/mcspeedrunvs/event/CompassListener.kt
@@ -0,0 +1,26 @@
+package io.github.rektinpieces.mcspeedrunvs.event
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import io.github.rektinpieces.mcspeedrunvs.data.SpeedrunGame
+
+class CompassListener(private val game: SpeedrunGame) : Listener {
+
+
+    fun changeCompassPosition() {
+        if (Player.)
+    }
+}

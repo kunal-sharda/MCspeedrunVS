7536dd5da58b47172b5cc31b1d75a1865f0f37f2
Kevin Liu <kevin@kliu.io>
2020-08-04 00:53:28 -0400

    Impl more of compass listener

--- src/main/kotlin/io/github/rektinpieces/mcspeedrunvs/Plugin.kt
+++ src/main/kotlin/io/github/rektinpieces/mcspeedrunvs/Plugin.kt
@@ -2,13 +2,22 @@ package io.github.rektinpieces.mcspeedrunvs
 
 import io.github.rektinpieces.mcspeedrunvs.commands.SpeedrunCommand
 import io.github.rektinpieces.mcspeedrunvs.data.SpeedrunGame
+import io.github.rektinpieces.mcspeedrunvs.event.CompassListener
 import io.github.rektinpieces.mcspeedrunvs.event.DragonKilledListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin() {
     override fun onEnable() {
         val game = SpeedrunGame(this)
-        server.pluginManager.registerEvents(DragonKilledListener(game), this)
+        val compassListener = CompassListener(game)
+        val dragonKilledListener = DragonKilledListener(game)
+
+        server.pluginManager.registerEvents(dragonKilledListener, this)
+        server.pluginManager.registerEvents(compassListener, this)
+
+        // Schedule compass position updating task
+        server.scheduler.runTaskTimer(this, compassListener::changeCompassPosition, 20, 20)
+
         getCommand("sr")!!.setExecutor(SpeedrunCommand(game))
 
         logger.info("Enabled MCSpeedrunVS")
--- src/main/kotlin/io/github/rektinpieces/mcspeedrunvs/event/CompassListener.kt
+++ src/main/kotlin/io/github/rektinpieces/mcspeedrunvs/event/CompassListener.kt
@@ -1,6 +1,8 @@
 package io.github.rektinpieces.mcspeedrunvs.event
 
 import io.github.rektinpieces.mcspeedrunvs.data.SpeedrunGame
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -15,6 +17,7 @@ enum class CompassMode {
 
 class CompassListener(private val game: SpeedrunGame) : Listener {
     private val compassModes = mutableMapOf<Player, CompassMode>()
+    private val selectIndex = mutableMapOf<Player, Int>().withDefault { 0 }
 
     // Left click: cycle through enemy team's nearest player
     // Right click: cycle through own team players
@@ -28,9 +31,11 @@ class CompassListener(private val game: SpeedrunGame) : Listener {
         when (event.action) {
             Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> {
                 compassModes[player] = CompassMode.ALLY_CYCLE
+                selectIndex[player] = selectIndex.getValue(player) + 1
             }
             Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> {
                 compassModes[player] = CompassMode.ENEMY_CYCLE
+                selectIndex[player] = selectIndex.getValue(player) + 1
             }
             Action.PHYSICAL -> {
                 Bukkit.broadcastMessage("Epic, see: https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/event/block/Action.html#PHYSICAL")
@@ -40,27 +45,43 @@ class CompassListener(private val game: SpeedrunGame) : Listener {
         }
     }
 
+    /**
+     * To be run by a Bukkit timer every tick.
+     */
     fun changeCompassPosition() {
         val teams = game.getTeams()
         val players = Bukkit.getOnlinePlayers()
         // Player must be in a team
         players.filter { teams[it] != null }
                 // Calculate compass position!
-
                 .forEach { player ->
                     // Find closest target of different team
-                    when (compassModes.getOrDefault(player, CompassMode.ALLY_CYCLE)) {
-                        CompassMode.ALLY_CYCLE -> TODO()
-                        CompassMode.ENEMY_CYCLE -> {
-                            val target = players
-                                    .filterNot { it == player }
-                                    .filter { teams[it] != teams[player] }
-                                    .minBy { player.location.distance(it.location) }
-                            // Returns if no other suitable target found
-                                    ?: return
+                    val targets = when (compassModes.getOrDefault(player, CompassMode.ALLY_CYCLE)) {
+                        CompassMode.ALLY_CYCLE -> players
+                                .filterNot { it == player }
+                                // Same team
+                                .filter { teams[it] == teams[player] }
+                                // Sorted by distance
+                                .sortedBy { player.location.distance(it.location) }
+                        CompassMode.ENEMY_CYCLE -> players
+                                .filterNot { it == player }
+                                .filter { teams[it] != teams[player] }
+                                .sortedBy { player.location.distance(it.location) }
+                    }
+                    if (targets.isEmpty()) {
+                        return
+                    }
 
-                            player.compassTarget = target.location
-                        }
+                    val target = targets[selectIndex.getValue(player) % targets.size]
+
+                    // Set the compass target
+                    player.compassTarget = target.location
+
+                    if (player.inventory.itemInMainHand.type == Material.COMPASS) {
+                        // Send action bar message showing the nearest player
+                        // TODO show compass mode, and add colors, and make sure this disappears properly
+                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+                                TextComponent("Nearest player: ${target.displayName}"))
                     }
                 }
     }
